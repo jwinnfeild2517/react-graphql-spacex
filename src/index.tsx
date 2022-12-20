41a575744fbb9abfe042e0f22ddee5a4135c68e2
Jenner Thomas <jthomas@jthomas-mbp.attlocal.net>
2022-12-20 15:06:55 -0500

    apollo grapghql client setup

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,40 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'https://api.spacex.land/graphql/',
+  cache: new InMemoryCache(),
+});
+
+
+// // query example client
+// client.query({
+//   query: gql`
+//     query GetLaunch {
+//       launch(id: "109") {
+//         id
+//         mission_name
+//         details
+//         links {
+//           flickr_images
+//           mission_patch
+//         }
+//       }
+//     }
+//   `,
+// }).then(({ data }) => console.log(data))
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
 );
 
 // If you want to start measuring performance in your app, pass a function
